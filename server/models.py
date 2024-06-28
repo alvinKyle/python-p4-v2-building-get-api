89d62d7c7a3eb8bdc8eead46d28c4d430aa4021d
alvinKyle <alvinkyle70@gmail.com>
2024-06-28 14:54:49 +0300

    updated

--- server/models.py
+++ server/models.py
@@ -14,8 +14,10 @@ metadata = MetaData(
 db = SQLAlchemy(metadata=metadata)
 
 
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = "games"
+
+    serialize_rules = ("-reviews.game",)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
@@ -27,12 +29,18 @@ class Game(db.Model):
 
     reviews = db.relationship("Review", back_populates="game")
 
+    # Association proxy to get users for this game through reviews
+    users = association_proxy("reviews", "user",
+                              creator=lambda user_obj: Review(user=user_obj))
+
     def __repr__(self):
         return f"<Game {self.title} for {self.platform}>"
 
 
 class Review(db.Model):
     __tablename__ = "reviews"
+
+    serialize_rules = ("-game.reviews", "-user.reviews",)
 
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
@@ -52,6 +60,8 @@ class Review(db.Model):
 
 class User(db.Model):
     __tablename__ = "users"
+
+    serialize_rules = ("-reviews.user",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
